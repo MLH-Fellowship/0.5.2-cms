cb9dfae23957414453e5199f9d53a3615245d542
Ebonne Cabarrus <42730922+ebonnecab@users.noreply.github.com>
2020-06-04 09:09:52 -0700

    storing changes

--- server/auth.py
+++ server/auth.py
@@ -3,8 +3,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template,
     request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
-#change file name later
-from flask_app.db import get_db
+from server.db import get_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
