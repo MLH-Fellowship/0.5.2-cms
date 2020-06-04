1988b680506e5a68a8d78f09c235d8af19cc2b5d
Amy <leiamy12@gmail.com>
2020-06-04 12:09:25 -0400

    add card media and modal

--- client/src/AllContacts.js
+++ client/src/AllContacts.js
@@ -3,17 +3,51 @@ import { FAKE_CONTACTS } from './data';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add'
+import Modal from '@material-ui/core/Modal'
+import { styled } from '@material-ui/core/styles';
 
 import './styles/App.css';
 
+const Avatar = styled(CardMedia)({
+    height: 250, 
+})
+
+class AddContactForm extends Component {
+    render() {
+        return (
+            <Modal
+                open={this.props.open}
+                onClose={this.props.onClose}
+            >
+                <p>
+                Hello World
+                </p>
+            </Modal>
+        )
+    }
+}
+
+
 export default class AllContacts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openForm: false,
+        }
+    }
+    
     render() {
         const cards = FAKE_CONTACTS.map((contact, i) => (
             <Card className='card-profile' variant='outlined'>
+                <Avatar
+                    image={contact.image}
+                    title={contact.name + '_avatar'}
+                />
                 <CardContent>
                     <Typography variant='h5' component='h2'>
                         {contact.name}
@@ -42,7 +76,11 @@ export default class AllContacts extends Component {
                     <main className='cards-container'>
                         {cards}
                     </main>
-                    <Fab className='fa-btn' color='primary' variant='extended'>
+                    <Fab 
+                        onClick={() => this.setState({ open: true })}
+                        className='fa-btn' 
+                        color='primary' 
+                        variant='extended'>
                         <AddIcon/> 
                         Add Contact
                     </Fab>
