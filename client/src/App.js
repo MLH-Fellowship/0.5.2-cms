507095b69ada7b7b7b9b4ed0132a13ec580b033a
Amy <leiamy12@gmail.com>
2020-06-04 23:59:26 -0400

    create PUT helper func

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import Home from './components/Home';
 import './styles/App.css';
+import { post, put } from './utilities';
 
 class App extends Component {
   render() {
